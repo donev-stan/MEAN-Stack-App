cfaf51047e6ebbe89ac2829b174b7c209c2a488c
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-24 17:01:27 +0200

    fetching initial posts

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.send("Hello from express");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: 1,
+      title: "Post 1",
+      content: "First post content bla",
+    },
+    {
+      id: 2,
+      title: "Post 2",
+      content: "Second post content bla",
+    },
+  ];
+
+  res.status(200).json({
+    message: "Post fetched successfully!",
+    posts,
+  });
 });
 
 module.exports = app;
