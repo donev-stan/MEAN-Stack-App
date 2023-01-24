f8db4a91c1d896dbb5e4669a19c7b57672f8c196
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-24 14:32:12 +0200

    Update material.module.ts

--- mean-app/src/app/shared/material/material.module.ts
+++ mean-app/src/app/shared/material/material.module.ts
@@ -4,10 +4,17 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [],
-  exports: [MatInputModule, MatFormFieldModule, MatCardModule, MatButtonModule],
+  exports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
