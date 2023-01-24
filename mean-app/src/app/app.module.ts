20b59571a047ef808193844bb29b8f63253ab659
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-24 14:40:31 +0200

    header

--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
   imports: [
     BrowserModule,
     FormsModule,
