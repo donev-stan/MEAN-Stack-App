7d6ff9b57dc212007c6961a7fcffe9eea74dead6
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-24 13:18:18 +0200

    Post Create Component

---
+++ mean-app/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
