7daff32d6bc7062d944053a29c951a37768fee03
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-29 02:11:56 +0200

    login and signup components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
@@ -7,6 +9,8 @@ const routes: Routes = [
   { path: 'list-post', component: PostListComponent },
   { path: 'create-post', component: PostCreateComponent },
   { path: 'edit-post/:postId', component: PostCreateComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
