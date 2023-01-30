e35a90268ddc350ad39607f3a5d39d1d94eadf30
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-30 11:29:59 +0200

    User Login on the BE

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const user = require("../models/user");
 
 const router = express.Router();
 
@@ -26,6 +28,38 @@ router.post("/signup", (req, res, next) => {
         });
       });
   });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "User not found",
+        });
+      }
+
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Invalid credentials",
+        });
+      }
+
+      const token = jwt.sign({ email: user.email, userId: user._id }, "secret-key-this-should-be-longer", { expiresIn: "1h" });
+
+      res.status(200).json({
+        message: "User logged in successfully",
+        user: user,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error,
+      });
+    });
 });
 
 module.exports = router;
