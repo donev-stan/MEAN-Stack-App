4325044f24f7ac5ee6f4a494bcb31fa61c4d2770
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-30 11:24:43 +0200

    Multer Config Abstraction

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,30 +1,10 @@
 const express = require("express");
 const multer = require("multer");
+const storage = require("../config/multer");
 
 const Post = require("../models/post");
 
 const router = express.Router();
-
-const MIME_TYPE_MAP = {
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
-  "image/png": "png",
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid MIME Type");
-    if (isValid) error = null;
-
-    callback(error, "images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    callback(null, name + "-" + Date.now() + "." + ext);
-  },
-});
 
 // All Document
 router.get("", (req, res, next) => {
